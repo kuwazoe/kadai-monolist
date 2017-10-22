af0387aa0d3ebc7963d412bbc02166287b938bc9
kuwazoe <hkuwazoe@ipcreate.co.jp>
2017-10-22 07:49:48 +0000

    ranking

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -19,6 +19,14 @@
                                 アイテムを追加
                             </a>
                         </li>
+                        
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <span class="glyphicon-signal" aria-hidden="true"></span>
+                                ランキング
+                                <span class=""create></span>
+                            </a>
+                        </li>
 
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
